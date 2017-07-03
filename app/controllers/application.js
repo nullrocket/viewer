9ebe0112a834c6a8543711842a96590622fb2ffc
nullrocket <doug.hill@passpack.com>
2017-07-03 13:39:48 -0700

    Added dist to git

---
+++ app/controllers/application.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+import {task} from "ember-concurrency";
+import {getAll} from 'viewer/api/api';
+export default Ember.Controller.extend({
+  results:null,
+  id:"",
+  lookup: task(function * () {
+  let results = yield getAll(this.get('id'));
+  this.set('results',results);
+  }).drop(),
+});
